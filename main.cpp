68966d17fc8052e6e294bb248e596b5997e54344
cawac <cawapl33@gmail.com>
2021-11-22 20:58:32 +0300

    ready project

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include "tag.h"
+int main() {
+    srand(time(NULL));
+    game tag ;
+    int changefield;
+    tag.create();
+    std::cout<<tag;
+    while (!tag.wincondition()) {
+        std::cin>>changefield;
+        tag.move(changefield);
+        std::cout<<tag;
+    }
+    std::cout<<"you are the champion!)";
+    return 0;
+}
